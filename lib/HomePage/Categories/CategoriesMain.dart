9feea3ec3e57e9a56c5708e85344b8d23403b8af
Shubhamshirva <shubhamshirva@gmail.com>
2023-04-10 19:46:17 +0530

    Alertdialog called in all bottombar

--- lib/HomePage/Categories/CategoriesMain.dart
+++ lib/HomePage/Categories/CategoriesMain.dart
@@ -169,29 +169,30 @@ class _CategoriesMainState extends State<CategoriesMain> {
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-      onWillPop: () async {
-        // SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-        final difference = DateTime.now().difference(timebackPressed);
-        final isExitWarning = difference >= Duration(seconds: 2);
+      onWillPop: () => _backbuttonpressed(context),
+      // async {
+      //   // SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+      //   final difference = DateTime.now().difference(timebackPressed);
+      //   final isExitWarning = difference >= Duration(seconds: 2);
 
-        timebackPressed = DateTime.now();
+      //   timebackPressed = DateTime.now();
 
-        if (isExitWarning) {
-          const message = "Press back again to exit";
-          // print("reached here");
-          Fluttertoast.showToast(
-            msg: message,
-            fontSize: 18,
-          );
+      //   if (isExitWarning) {
+      //     const message = "Press back again to exit";
+      //     // print("reached here");
+      //     Fluttertoast.showToast(
+      //       msg: message,
+      //       fontSize: 18,
+      //     );
 
-          return false;
-        } else {
-          Fluttertoast.cancel();
+      //     return false;
+      //   } else {
+      //     Fluttertoast.cancel();
 
-          SystemNavigator.pop();
-          return true;
-        }
-      },
+      //     SystemNavigator.pop();
+      //     return true;
+      //   }
+      // },
       child: Scaffold(
         key: _key,
         // drawer: NavDrawer(),
@@ -425,5 +426,70 @@ class _CategoriesMainState extends State<CategoriesMain> {
         )
       ],
     );
+  }
+  
+   Future<bool> _backbuttonpressed(BuildContext context) async {
+    bool exitapp = await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Padding(
+          padding:  EdgeInsets.all(15.w),
+          child: AlertDialog(
+             shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.r)),
+          insetPadding: const EdgeInsets.symmetric(vertical: 10),
+            title:Text(
+            "Exit App",
+            style: TextStyle(
+                fontFamily: 'Studio Pro',
+                fontWeight: FontWeight.bold,
+                fontSize: 18.sp,
+                color: const Color(0xff3B3F43)),
+          ),
+            content: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: Text(
+              "Are you sure you want to Exit App?",
+              style: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontSize: 16.sp,
+                  color: const Color(0xff54595F)),
+            ),
+          ),
+            actions: [
+             InkWell(
+              onTap: () {
+                Get.back();
+              },
+              child: Text(
+                "No",
+                style: TextStyle(
+                    fontFamily: "Roboto",
+                    fontWeight: FontWeight.w500,
+                    fontSize: 16.sp,
+                    color: const Color(0xff000000)),
+              ),
+            ),
+            sizedBoxWidth(15.sp),
+            InkWell(
+              onTap: () {
+                SystemNavigator.pop();
+              },
+              child: Text(
+                "Yes",
+                style: TextStyle(
+                    fontFamily: "Roboto",
+                    fontWeight: FontWeight.w500,
+                    fontSize: 16.sp,
+                    color: const Color(0xffB90101)),
+              ),
+            ),
+            sizedBoxWidth(15.sp),
+            ],
+          ),
+        );
+      },
+    );
+    return exitapp ?? false;
   }
 }
