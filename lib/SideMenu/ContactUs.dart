a27775d40facf0ef4c2f4b2136e0bcb592894ca5
Nadeem Shaikh <nadeemns1104@gmail.com>
2023-04-13 13:09:53 +0530

    contact us validation

--- lib/SideMenu/ContactUs.dart
+++ lib/SideMenu/ContactUs.dart
@@ -17,6 +17,7 @@ class ContactUs extends StatefulWidget {
 }
 
 class _ContactUsState extends State<ContactUs> {
+  final GlobalKey<FormState> _form = GlobalKey<FormState>();
   final residentialstatustexteditingcontroller = TextEditingController();
 
   @override
@@ -30,6 +31,7 @@ class _ContactUsState extends State<ContactUs> {
       ),
       body: Center(
         child: Form(
+          key: _form,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -71,6 +73,12 @@ class _ContactUsState extends State<ContactUs> {
                       height: 15.h,
                     ),
                     CustomTextFormField(
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Please Enter Your Name";
+                          }
+                          return null;
+                        },
                         hintText: "  Enter Full Name",
                         validatorText: "Please Enter Full Name"),
                     SizedBox(
@@ -92,6 +100,17 @@ class _ContactUsState extends State<ContactUs> {
                       height: 15.h,
                     ),
                     CustomTextFormField(
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return "Please Enter Your Email";
+                          }
+                          if (!RegExp(
+                                  r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
+                              .hasMatch(value)) {
+                            return 'Enter a Valid Email address';
+                          }
+                          return null;
+                        },
                         hintText: "  Enter Email Address",
                         validatorText: "Please Enter Email Address"),
                     SizedBox(height: 25.h),
@@ -111,6 +130,17 @@ class _ContactUsState extends State<ContactUs> {
                       height: 15.h,
                     ),
                     CustomTextFormField(
+                        texttype: TextInputType.phone,
+                        validator: (value) {
+                          if (value == value.isEmpty) {
+                            return 'Mobile number is required';
+                          } else if (!RegExp(r'(^(?:[+0]9)?[0-9]{10}$)')
+                              .hasMatch(value)) {
+                            return 'Enter valid mobile number';
+                          }
+                          // v3 = true;
+                          return null;
+                        },
                         hintText: "  Enter Mobile Number",
                         validatorText: "Please Enter Mobile Number"),
                     SizedBox(height: 25.h),
@@ -130,6 +160,12 @@ class _ContactUsState extends State<ContactUs> {
                       height: 15.h,
                     ),
                     CustomTextFormField(
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return 'Subject is required';
+                          }
+                          return null;
+                        },
                         hintText: "  Enter Subject",
                         validatorText: "Please Enter Subject"),
                     SizedBox(
@@ -169,7 +205,7 @@ class _ContactUsState extends State<ContactUs> {
                               const BorderSide(color: Colors.red, width: 1),
                         ),
                         hintStyle: TextStyle(
-                            color: Color(0x80000000), fontSize: 16.sp),
+                            color: const Color(0x80000000), fontSize: 16.sp),
                         hintText: "  Message",
                       ),
                       minLines: 5,
@@ -179,41 +215,46 @@ class _ContactUsState extends State<ContactUs> {
                     CustomNextButton(
                       text: "Send Now",
                       ontap: () {
-                        Timer(Duration(seconds: 2),
-                            () => Get.offAllNamed('/EntryPoint', arguments: 0));
-                        showModalBottomSheet(
-                          isScrollControlled: true,
-                          context: context,
-                          shape: const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(30),
-                              topRight: Radius.circular(30),
-                            ),
-                          ),
-                          builder: (context) {
-                            return Container(
-                              margin: const EdgeInsets.symmetric(
-                                  horizontal: 16, vertical: 24),
-                              child: Column(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  Image.asset("assets/images/letter.png"),
-                                  SizedBox(
-                                    height: 28.h,
-                                  ),
-                                  Text(
-                                    "Thankyou for contacting.\nWill reach back to you",
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                        color: Color(0xFF444444),
-                                        fontSize: 20.sm,
-                                        fontFamily: "Poppins"),
-                                  ),
-                                ],
+                        final isValid = _form.currentState?.validate();
+                        if (isValid!) {
+                          Timer(
+                              const Duration(seconds: 2),
+                              () =>
+                                  Get.offAllNamed('/EntryPoint', arguments: 0));
+                          showModalBottomSheet(
+                            isScrollControlled: true,
+                            context: context,
+                            shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(30),
+                                topRight: Radius.circular(30),
                               ),
-                            );
-                          },
-                        );
+                            ),
+                            builder: (context) {
+                              return Container(
+                                margin: const EdgeInsets.symmetric(
+                                    horizontal: 16, vertical: 24),
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Image.asset("assets/images/letter.png"),
+                                    SizedBox(
+                                      height: 28.h,
+                                    ),
+                                    Text(
+                                      "Thankyou for contacting.\nWill reach back to you",
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          color: Color(0xFF444444),
+                                          fontSize: 20.sm,
+                                          fontFamily: "Poppins"),
+                                    ),
+                                  ],
+                                ),
+                              );
+                            },
+                          );
+                        }
                       },
                     ),
                     SizedBox(
