532cc7eb07b750c009580d23d7f237d9a20f5cd1
shams7khan <khanshamshad648@gmail.com>
2023-05-03 20:53:18 +0530

    changes by client

--- lib/HomePage/Categories/revenue_based_financing/revenue_based_learn_more.dart
+++ lib/HomePage/Categories/revenue_based_financing/revenue_based_learn_more.dart
@@ -295,6 +295,7 @@ class _RevenueBasedLearnMoreState extends State<RevenueBasedLearnMore> {
             "Raising capital through RBF is easier, and faster as mostly, it does not require any collateral or personal guarantee."
       },
     ];
+  
     return Container(
       decoration: BoxDecoration(
           border: Border.all(color: Colors.black.withOpacity(0.2)),
