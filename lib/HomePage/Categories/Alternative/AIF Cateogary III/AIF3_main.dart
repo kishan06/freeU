66b24d695f76c3851ec49ec56d94c51e5812a1f7
Nadeem Shaikh <nadeemns1104@gmail.com>
2023-05-12 18:01:05 +0530

    minor changes

---
+++ lib/HomePage/Categories/Alternative/AIF Cateogary III/AIF3_main.dart
@@ -0,0 +1,216 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:freeu/Utils/texts.dart';
+import 'package:freeu/common/page_animation.dart';
+import 'package:freeu/common/sized_box.dart';
+import 'package:get/get.dart';
+
+import 'AlternaticeCateogries3.dart';
+import 'Cat3VerticalSlider.dart';
+
+class AIF3Main extends StatefulWidget {
+  const AIF3Main({super.key});
+
+  @override
+  State<AIF3Main> createState() => _AIF3MainState();
+}
+
+class _AIF3MainState extends State<AIF3Main> {
+  final residentialstatustexteditingcontroller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xff008A5B),
+      appBar: AppBar(
+        backgroundColor: const Color(0xff008A5B),
+        elevation: 0,
+        titleSpacing: 0,
+        leading: IconButton(
+          onPressed: () {
+            Get.back();
+          },
+          icon: const Icon(
+            Icons.arrow_back,
+          ),
+          iconSize: 26,
+          color: const Color(0XFFFFFFFF),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    "Category III",
+                    style: TextStyle(
+                        fontFamily: "Poppins",
+                        fontSize: 25.sp,
+                        color: const Color(0XFFFFFFFF),
+                        fontWeight: FontWeight.w500),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 16.h,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 20.h,
+                      ),
+                      Text(
+                        "As per SEBI Regulations Category III AIF aims at short-term high returns by employing diverse and complex trading strategies. It may also employ leverage including through investment in listed or unlisted derivatives ",
+                        style: TextStyle(
+                          color: const Color(0XFFFFFFFF),
+                          fontSize: 18.sp,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 9.h,
+                      ),
+                      OpenContainerWrappers(
+                        openBuild: const AlternaticeCateogries3(),
+                        closeBuild: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text(
+                              "Learn more",
+                              style: TextStyle(
+                                  color: const Color(0XFFFFFFFF),
+                                  fontSize: 16.sp,
+                                  decoration: TextDecoration.underline),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20.h,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Image.asset(
+                            "assets/images/privatecategories.png",
+                            width: 70.w,
+                            height: 70.w,
+                          ),
+                          SizedBox(
+                            width: 25.w,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Expected Return (IRR)",
+                                  style: TextStyle(
+                                    color: const Color(0XFFFBFBFB),
+                                    fontSize: 16.sp,
+                                    fontFamily: "Poppins",
+                                  )),
+                              text20White("~ 12-14% p.a.")
+                            ],
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 30.h,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Image.asset(
+                            "assets/images/privateequitytime.png",
+                            width: 70.w,
+                            height: 70.w,
+                          ),
+                          SizedBox(
+                            width: 25.w,
+                          ),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text("Suggested Investment Horizon",
+                                    style: TextStyle(
+                                      color: const Color(0XFFFBFBFB),
+                                      fontSize: 16.sp,
+                                      fontFamily: "Poppins",
+                                    )),
+                                text20White("More than 4 Years")
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 30.h,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Image.asset(
+                            "assets/images/funding.png",
+                            width: 70.w,
+                            height: 70.w,
+                          ),
+                          SizedBox(
+                            width: 25.w,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Minimum Investment",
+                                  style: TextStyle(
+                                    color: const Color(0XFFFBFBFB),
+                                    fontSize: 16.sp,
+                                    fontFamily: "Poppins",
+                                  )),
+                              text20White("1 Crore")
+                            ],
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 40.h,
+                      ),
+                      OpenContainerWrappers(
+                        openBuild: const Cat3VerticalSlider(),
+                        closeBuild: Container(
+                          decoration: BoxDecoration(
+                              color: const Color(0XFFFBFBFB),
+                              borderRadius: BorderRadius.circular(10.r)),
+                          width: double.infinity,
+                          height: 50.h,
+                          child: Center(
+                            child: Text(
+                              "View Cateogries",
+                              style: TextStyle(
+                                color: const Color(0XFF000000),
+                                fontSize: 18.sp,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      sizedBoxHeight(38.h),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+    // );
+  }
+}
---
+++ lib/HomePage/Categories/Alternative/AIF_main.dart
@@ -0,0 +1,216 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:freeu/Utils/colors.dart';
+import 'package:freeu/Utils/texts.dart';
+import 'package:freeu/common/page_animation.dart';
+import 'package:freeu/common/sized_box.dart';
+import 'package:get/get.dart';
+import '../AlternativeInvestment.dart';
+import 'AlternativeCategories.dart';
+
+class AIFMain extends StatefulWidget {
+  const AIFMain({super.key});
+
+  @override
+  State<AIFMain> createState() => _AIFMainState();
+}
+
+class _AIFMainState extends State<AIFMain> {
+  final residentialstatustexteditingcontroller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.redL_BE0F02,
+      appBar: AppBar(
+        backgroundColor: AppColors.redL_BE0F02,
+        elevation: 0,
+        titleSpacing: 0,
+        leading: IconButton(
+          onPressed: () {
+            Get.back();
+          },
+          icon: const Icon(
+            Icons.arrow_back,
+          ),
+          iconSize: 26,
+          color: const Color(0XFFFFFFFF),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    "Alternative Investment Funds",
+                    style: TextStyle(
+                        fontFamily: "Poppins",
+                        fontSize: 25.sp,
+                        color: const Color(0XFFFFFFFF),
+                        fontWeight: FontWeight.w500),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 16.h,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 20.h,
+                      ),
+                      Text(
+                        """Alternate Investment Funds or AIFs are privately pooled investment vehicles that collect funds from investors, under specific guidelines of the SEBI (Securities and Exchange Board of India). It includes venture debt, hedge funds, real estate funds, PIPE investments, private equity, etc.""",
+                        style: TextStyle(
+                          color: const Color(0XFFFFFFFF),
+                          fontSize: 18.sp,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 9.h,
+                      ),
+                      OpenContainerWrappers(
+                        openBuild: const AlternativeInsvestment(),
+                        closeBuild: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text(
+                              "Learn more",
+                              style: TextStyle(
+                                  color: const Color(0XFFFFFFFF),
+                                  fontSize: 16.sp,
+                                  decoration: TextDecoration.underline),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20.h,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Image.asset(
+                            "assets/images/privatecategories.png",
+                            width: 70.w,
+                            height: 70.w,
+                          ),
+                          SizedBox(
+                            width: 25.w,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Expected Return (IRR)",
+                                  style: TextStyle(
+                                    color: const Color(0XFFFBFBFB),
+                                    fontSize: 16.sp,
+                                    fontFamily: "Poppins",
+                                  )),
+                              text20White("~ 12-24% p.a.")
+                            ],
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 30.h,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Image.asset(
+                            "assets/images/privateequitytime.png",
+                            width: 70.w,
+                            height: 70.w,
+                          ),
+                          SizedBox(
+                            width: 25.w,
+                          ),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text("Suggested Investment Horizon",
+                                    style: TextStyle(
+                                      color: const Color(0XFFFBFBFB),
+                                      fontSize: 16.sp,
+                                      fontFamily: "Poppins",
+                                    )),
+                                text20White("More than 5 Years")
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 30.h,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Image.asset(
+                            "assets/images/funding.png",
+                            width: 70.w,
+                            height: 70.w,
+                          ),
+                          SizedBox(
+                            width: 25.w,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Minimum Investment",
+                                  style: TextStyle(
+                                    color: const Color(0XFFFBFBFB),
+                                    fontSize: 16.sp,
+                                    fontFamily: "Poppins",
+                                  )),
+                              text20White("1 Crore")
+                            ],
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 40.h,
+                      ),
+                      OpenContainerWrappers(
+                        openBuild: const AlternativeCategories(),
+                        closeBuild: Container(
+                          decoration: BoxDecoration(
+                              color: const Color(0XFFFBFBFB),
+                              borderRadius: BorderRadius.circular(10.r)),
+                          width: double.infinity,
+                          height: 50.h,
+                          child: Center(
+                            child: Text(
+                              "View Cateogries",
+                              style: TextStyle(
+                                color: const Color(0XFF000000),
+                                fontSize: 18.sp,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      sizedBoxHeight(38.h),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+    // );
+  }
+}
