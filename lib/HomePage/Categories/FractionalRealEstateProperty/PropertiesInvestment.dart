d77b777485c8215e3f1a86e40184c9ad578dea12
Shubhamshirva <shubhamshirva@gmail.com>
2023-08-28 17:56:57 +0530

    fracional details page api integrated

--- lib/HomePage/Categories/FractionalRealEstateProperty/PropertiesInvestment.dart
+++ lib/HomePage/Categories/FractionalRealEstateProperty/PropertiesInvestment.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:freeu/Utils/texts.dart';
 import 'package:freeu/common/Other%20Commons/customNextButton.dart';
 import 'package:freeu/common/Other%20Commons/signupAppbar.dart';
 import 'package:freeu/common/Other%20Commons/sized_box.dart';
 import 'package:freeu/controllers/entry_point_controller.dart';
+import 'package:freeu/viewModel/FractionalDetails.dart';
+import 'package:freeu/viewModel/FractionalProperties.dart';
 import 'package:get/get.dart';
 
 class PropertiesInvestment extends StatefulWidget {
-  final int pageIndex;
+  final String slug;
 
-  PropertiesInvestment({super.key, required this.pageIndex});
+  PropertiesInvestment({super.key, required this.slug});
 
   @override
   State<PropertiesInvestment> createState() => _PropertiesInvestmentState();
@@ -18,6 +21,13 @@ class PropertiesInvestment extends StatefulWidget {
 
 class _PropertiesInvestmentState extends State<PropertiesInvestment> {
   final controllerEntryPoint = Get.put(EntryPointController());
+  late Future myfuture;
+
+  @override
+  void initState() {
+    myfuture = FractionaDetails().FractionaDetailsAPI(widget.slug);
+    super.initState();
+  }
 
   List productDetails = [
     {
@@ -115,101 +125,345 @@ class _PropertiesInvestmentState extends State<PropertiesInvestment> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomSignupAppBar(
-        titleTxt: "",
-        bottomtext: false,
-      ),
-      bottomNavigationBar: Padding(
-        padding: EdgeInsets.fromLTRB(16.w, 5.h, 16.w, 10.h),
-        child: CustomNextButton(
-            ontap: () {
-              if (controllerEntryPoint.logedIn!) {
-                investNow();
-              } else {
-                Get.toNamed("/login");
-              }
-            },
-            text: 'Invest now'),
-      ),
-      body: SafeArea(
-          child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16.w),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              sizedBoxHeight(10.h),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  sizedBoxWidth(5.w),
-                  Container(
-                    child: SvgPicture.asset(
-                      "assets/images/property.svg",
-                      width: 80.w,
-                      height: 54.h,
-                    ),
-                  ),
-                  SizedBox(
-                    width: 20.h,
-                  ),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width - 132.w,
-                    height: 85.h,
-                    child: Text(
-                      productDetails[0]['Company Name'][widget.pageIndex],
-                      style: TextStyle(
-                          fontSize: 20.sp, fontWeight: FontWeight.w500),
-                    ),
-                  ),
-                ],
-              ),
-              sizedBoxHeight(24.h),
-              SizedBox(
-                height: MediaQuery.of(context).size.height - 245.h,
-                child: ListView.separated(
-                    itemBuilder: (context, index) {
-                      return Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            productDetails[0]['header'][index],
-                            style: TextStyle(
-                              fontWeight: FontWeight.w500,
-                              fontSize: 20.sp,
-                              color: Color(0XFF3A4856),
-                            ),
-                          ),
-                          Divider(
-                            height: 25.h,
-                            thickness: 1.h,
-                            color: Colors.grey.shade400,
-                          ),
-                          Text(
-                            productDetails[0]['content'][widget.pageIndex]
-                                [index],
-                            style: TextStyle(
-                              fontSize: 18.sp,
-                              color: Color(0XFF272424),
-                            ),
-                          ),
-                          sizedBoxHeight(28.h),
-                        ],
-                      );
-                    },
-                    separatorBuilder: (context, index) {
-                      return SizedBox(
-                        height: 0.h,
-                      );
-                    },
-                    itemCount: productDetails[0]['header'].length),
-              )
-            ],
-          ),
+        appBar: CustomSignupAppBar(
+          titleTxt: "",
+          bottomtext: false,
         ),
-      )),
-    );
+        bottomNavigationBar: Padding(
+          padding: EdgeInsets.fromLTRB(16.w, 5.h, 16.w, 10.h),
+          child: CustomNextButton(
+              ontap: () {
+                if (controllerEntryPoint.logedIn!) {
+                  investNow();
+                } else {
+                  Get.toNamed("/login");
+                }
+              },
+              text: 'Invest now'),
+        ),
+        body: FutureBuilder(
+          future: myfuture,
+          builder: (ctx, snapshot) {
+            if (snapshot.data == null) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [CircularProgressIndicator()],
+              );
+            }
+            if (snapshot.connectionState == ConnectionState.done) {
+              if (snapshot.hasError) {
+                return Center(
+                  child: Text(
+                    '${snapshot.error} occured',
+                    style: TextStyle(fontSize: 18.spMin),
+                  ),
+                );
+              }
+            }
+            return _buildBody(
+              context,
+            );
+          },
+        ));
+  }
+
+  Widget _buildBody(context) {
+    return SafeArea(
+        child: Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16.w),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            sizedBoxHeight(10.h),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                sizedBoxWidth(5.w),
+                Container(
+                  child: SvgPicture.asset(
+                    "assets/images/property.svg",
+                    width: 80.w,
+                    height: 54.h,
+                  ),
+                ),
+                SizedBox(
+                  width: 20.h,
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width - 132.w,
+                  height: 85.h,
+                  child: Text(
+                    fractionalDetailsObj!.data!.slug ?? "",
+                    style:
+                        TextStyle(fontSize: 20.sp, fontWeight: FontWeight.w500),
+                  ),
+                ),
+              ],
+            ),
+            sizedBoxHeight(24.h),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                textA4856_20500("Property details(Name and Location)"),
+                Divider(
+                  height: 25.h,
+                  thickness: 1.h,
+                  color: Colors.grey.shade400,
+                ),
+                text272424_18(
+                  fractionalDetailsObj!.data!.propertyNameAndLocation ?? "",
+                ),
+                sizedBoxHeight(20.h),
+                textA4856_20500("Property Description"),
+                Divider(
+                  height: 25.h,
+                  thickness: 1.h,
+                  color: Colors.grey.shade400,
+                ),
+                text272424_18(
+                  fractionalDetailsObj!.data!.propertyDescription ?? "",
+                ),
+                
+                sizedBoxHeight(20.h),
+                textA4856_20500("Property Grade"),
+                Divider(
+                  height: 25.h,
+                  thickness: 1.h,
+                  color: Colors.grey.shade400,
+                ),
+                text272424_18(
+                  fractionalDetailsObj!.data!.propertyGrade ?? "",
+                ),
+                
+                sizedBoxHeight(20.h),
+                textA4856_20500("Asset Type"),
+                Divider(
+                  height: 25.h,
+                  thickness: 1.h,
+                  color: Colors.grey.shade400,
+                ),
+                text272424_18(
+                  fractionalDetailsObj!.data!.assetType ?? "",
+
+                ),
+                
+                sizedBoxHeight(20.h),
+                textA4856_20500("Tenant"),
+                
+                Divider(
+                  height: 25.h,
+                  thickness: 1.h,
+                  color: Colors.grey.shade400,
+                ),
+                text272424_18(
+                  fractionalDetailsObj!.data!.tenant ?? "",
+              
+                ),
+                sizedBoxHeight(20.h),
+                textA4856_20500("Deal Size (in Crore)"),
+                Divider(
+                  height: 25.h,
+                  thickness: 1.h,
+                  color: Colors.grey.shade400,
+                ),
+                text272424_18(
+                  fractionalDetailsObj!.data!.dealSizeInCrore ?? "",
+                ),
+                sizedBoxHeight(20.h),
+                textA4856_20500("Minimum Investment"),
+                Divider(
+                  height: 25.h,
+                  thickness: 1.h,
+                  color: Colors.grey.shade400,
+                ),
+                text272424_18(
+                  fractionalDetailsObj!.data!.minimumInvestment ?? "No data",
+
+                ),
+                sizedBoxHeight(20.h),
+                textA4856_20500("Coupon Rate on CCD"),
+                
+                Divider(
+                  height: 25.h,
+                  thickness: 1.h,
+                  color: Colors.grey.shade400,
+                ),
+                text272424_18(
+                  fractionalDetailsObj!.data!.couponRateOnCcd ?? "",
+
+                ),
+                sizedBoxHeight(20.h),
+                textA4856_20500("Rental Escalation"),
+                Divider(
+                  height: 25.h,
+                  thickness: 1.h,
+                  color: Colors.grey.shade400,
+                ),
+                text272424_18(
+                  fractionalDetailsObj!.data!.rentalEscalation ?? "",
+
+                ),
+                sizedBoxHeight(20.h),
+                textA4856_20500("Capital Appreciation"),
+                Divider(
+                  height: 25.h,
+                  thickness: 1.h,
+                  color: Colors.grey.shade400,
+                ),
+                text272424_18(
+                  fractionalDetailsObj!.data!.capitalAppreciation ?? "",
+
+                ),
+                sizedBoxHeight(20.h),
+                textA4856_20500("Expected IRR"),
+                Divider(
+                  height: 25.h,
+                  thickness: 1.h,
+                  color: Colors.grey.shade400,
+                ),
+                text272424_18(
+                  fractionalDetailsObj!.data!.expectedIrr ?? "",
+                ),
+                sizedBoxHeight(20.h),
+                textA4856_20500("CAGR"),
+                
+                Divider(
+                  height: 25.h,
+                  thickness: 1.h,
+                  color: Colors.grey.shade400,
+                ),
+                text272424_18(
+                  fractionalDetailsObj!.data!.cagr ?? "",
+                ),
+                sizedBoxHeight(20.h),
+                textA4856_20500("Minimum Investor LockIn"),
+                Divider(
+                  height: 25.h,
+                  thickness: 1.h,
+                  color: Colors.grey.shade400,
+                ),
+                text272424_18(
+                  fractionalDetailsObj!.data!.minimumInvestmentLockin ?? "",
+                ),
+                sizedBoxHeight(20.h),
+                textA4856_20500("Tenant Lease Term"),
+                Divider(
+                  height: 25.h,
+                  thickness: 1.h,
+                  color: Colors.grey.shade400,
+                ),
+                text272424_18(
+                  fractionalDetailsObj!.data!.tenantLeaseTerm ?? "",
+                ),
+                sizedBoxHeight(20.h),
+                textA4856_20500("Tenant Lock In"),
+                Divider(
+                  height: 25.h,
+                  thickness: 1.h,
+                  color: Colors.grey.shade400,
+                ),
+                text272424_18(
+                  fractionalDetailsObj!.data!.tenantLockIn ?? "",
+                  
+                ),
+                sizedBoxHeight(20.h),
+                textA4856_20500("Tenant Security Deposit"),
+                Divider(
+                  height: 25.h,
+                  thickness: 1.h,
+                  color: Colors.grey.shade400,
+                ),
+                text272424_18(
+                  fractionalDetailsObj!.data!.tenantSecurityDeposit ?? "",
+                ),
+                sizedBoxHeight(20.h),
+                textA4856_20500("Annual Management Fee"),
+                Divider(
+                  height: 25.h,
+                  thickness: 1.h,
+                  color: Colors.grey.shade400,
+                ),
+                text272424_18(
+                  fractionalDetailsObj!.data!.annualManagementFee ?? "",
+                ),
+                sizedBoxHeight(20.h),
+                textA4856_20500("Performance Fee"),
+                Divider(
+                  height: 25.h,
+                  thickness: 1.h,
+                  color: Colors.grey.shade400,
+                ),
+                text272424_18(
+                  fractionalDetailsObj!.data!.performanceFees ?? "",),
+                sizedBoxHeight(20.h),
+                textA4856_20500("Hurdle rate"),
+                Divider(
+                  height: 25.h,
+                  thickness: 1.h,
+                  color: Colors.grey.shade400,
+                ),
+                text272424_18(
+                  fractionalDetailsObj!.data!.hurdleRate ?? "",),
+                sizedBoxHeight(20.h),
+              ],
+            ),
+
+            // SizedBox(
+            //   height: MediaQuery.of(context).size.height - 245.h,
+            //   child: ListView.separated(
+            //       itemBuilder: (context, index) {
+            //         return Column(
+            //           mainAxisAlignment: MainAxisAlignment.start,
+            //           crossAxisAlignment: CrossAxisAlignment.start,
+            //           children: [
+            //             Text(
+            //               productDetails[0]['header'][index],
+            //               style: TextStyle(
+            //                 fontWeight: FontWeight.w500,
+            //                 fontSize: 20.sp,
+            //                 color: Color(0XFF3A4856),
+            //               ),
+            //             ),
+            //             Divider(
+            //               height: 25.h,
+            //               thickness: 1.h,
+            //               color: Colors.grey.shade400,
+            //             ),
+            //             Text(
+            //               fractionalRealEstateObj!
+            //                       .data?[index]
+            //                       .fractionalRealEstate!
+            //                       .propertyNameAndLocation! ??
+            //                   "",
+            //               // productDetails[0]['content']
+            //               //     // [widget.pageIndex]
+            //               //     [index],
+            //               style: TextStyle(
+            //                 fontSize: 18.sp,
+            //                 color: Color(0XFF272424),
+            //               ),
+            //             ),
+            //             sizedBoxHeight(28.h),
+            //           ],
+            //         );
+            //       },
+            //       separatorBuilder: (context, index) {
+            //         return SizedBox(
+            //           height: 0.h,
+            //         );
+            //       },
+            //       itemCount: fractionalRealEstateObj!.data!.length
+            //       // productDetails[0]['header'].length
+            //       ),
+            // )
+          
+          ],
+        ),
+      ),
+    ));
   }
 
   void investNow() {
